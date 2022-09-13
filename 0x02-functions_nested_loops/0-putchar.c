1548273a290cf6161cdbb96e4c5c9f43aff3099d
abiodun ajibola <securemedjay@gmail.com>
2022-09-13 18:55:06 -0400

    modify 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
- * main - Print "_putchar" followed by a new line.
+ * main - Print _putchar followed by a new line.
+ * Description: Program should return 0.
  * Return: 0
  */
 int main(void)
 {
-	char _putchar[] = "_putchar";
+	char word[] = "_putchar";
 	int i = 0;
 
-	while (_putchar[i] != '\0')
+	while (word[i] != '\0')
 	{
-		_putchar(_putchar[i]);
+		_putchar(word[i]);
 		i++;
 	}
 	_putchar('\n');
