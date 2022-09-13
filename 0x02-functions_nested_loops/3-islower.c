0407397c3ab843a0eb681ecf3b00e54f14d36efa
abiodun ajibola <securemedjay@gmail.com>
2022-09-13 18:19:36 -0400

    modify 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  * _islower - Return 1 if letter is lowercase, 0 if not.
  * Return: 1 if lowercase, 0 if not lowercase
- * @c - int data type
+ * @c: int data type
  */
 int _islower(int c)
 {
